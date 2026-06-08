a1daf732ca60a89508be2f6b4504067f02a0d2b3
Agni-Kancharla <agnikancharla2006@gmail.com>
2026-06-08 16:05:31 +0530

    Added JavaScript interactivity and validation

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Portfolio Website Loaded");
+// Variables & Array
+let skills = ["HTML", "CSS", "JavaScript"];
+
+// Loop
+skills.forEach(skill => console.log(skill));
+
+// DOM Manipulation
+document.querySelector(".btn2").addEventListener("click", () => {
+    alert("Opening Contact Section");
+});
+
+// Form Validation
+document.querySelector(".contact-form").addEventListener("submit", function(e) {
+    let name = document.querySelector("input[type='text']").value;
+    let email = document.querySelector("input[type='email']").value;
+
+    if (name === "" || email === "") {
+        alert("Please fill all fields!");
+        e.preventDefault();
+    }
+});
